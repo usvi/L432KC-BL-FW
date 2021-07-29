a7ba2dbd6b9e829f765edc0aeb4f96a525cec869
Janne Paalijarvi <jpaalija@gmail.com>
2021-07-30 00:53:13 +0300

    Bootloader blinks LED with high frequency to indicate bootloader is running

--- L432KC_Bootloader/Core/Src/main.c
+++ L432KC_Bootloader/Core/Src/main.c
@@ -63,41 +63,25 @@ static void MX_GPIO_Init(void);
   */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  uint32_t u32LedCounter = 0;
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  /* USER CODE BEGIN 2 */
 
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+  // Run high frequency for a brief while, then jump
+  for (u32LedCounter = 0; u32LedCounter < 0x120000; u32LedCounter++)
+  {
+    if ((u32LedCounter % 0xFFFF) == 0)
+    {
+      HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
+    }
+  }
+  // Leave LED off
+  HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
+  // Deinit and jump
   while (1)
   {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
   }
-  /* USER CODE END 3 */
 }
 
 /**
