1a824908db9003402307389a6bb22c854aa6d6a1
Janne Paalijarvi <jpaalija@gmail.com>
2021-07-30 23:22:15 +0300

    Jump to main firmware works! But only to 0x8005000 address.

--- L432KC_Bootloader/Core/Src/main.c
+++ L432KC_Bootloader/Core/Src/main.c
@@ -77,7 +77,8 @@ int main(void)
   // Leave LED off
   HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
   // Deinit and jump
-  vL432kc_DeInitAndJump(0); // 0 here => bootloader jumps to itself :)
+  //vL432kc_DeInitAndJump(0); // 0 here => bootloader jumps to itself :)
+  vL432kc_DeInitAndJump(0x8005000); // Here actual firmware address
 }
 
 /**
