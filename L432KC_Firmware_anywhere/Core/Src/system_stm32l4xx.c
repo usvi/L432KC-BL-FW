4e69f4ef1d840b3bb0d3ebdbe9bf477d42c85984
Janne Paalijarvi <jpaalija@gmail.com>
2021-07-30 23:16:34 +0300

    Firmware now runs from 0x8005000 with the help of debugger. Upon full reset, bootloader runs.

--- L432KC_Firmware_anywhere/Core/Src/system_stm32l4xx.c
+++ L432KC_Firmware_anywhere/Core/Src/system_stm32l4xx.c
@@ -126,6 +126,8 @@
      remap of boot address selected */
 /* #define USER_VECT_TAB_ADDRESS */
 
+#define USER_VECT_TAB_ADDRESS
+
 #if defined(USER_VECT_TAB_ADDRESS)
 /*!< Uncomment the following line if you need to relocate your vector Table
      in Sram else user remap will be done in Flash. */
@@ -139,7 +141,7 @@
 #else
 #define VECT_TAB_BASE_ADDRESS   FLASH_BASE      /*!< Vector Table base address field.
                                                      This value must be a multiple of 0x200. */
-#define VECT_TAB_OFFSET         0x00000000U     /*!< Vector Table base offset field.
+#define VECT_TAB_OFFSET         0x00005000U     /*!< Vector Table base offset field.
                                                      This value must be a multiple of 0x200. */
 #endif /* VECT_TAB_SRAM */
 #endif /* USER_VECT_TAB_ADDRESS */
